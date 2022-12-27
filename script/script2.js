6c1fc7995d71e61d2036fe8716ae9240589aceee
elcorre <correa755@hotmail.com>
2022-12-26 22:16:03 -0300

    Ingreso de turnos completo, eliminación de ultimo turno completo, comenzando con los creates y appends

--- script/script2.js
+++ script/script2.js
@@ -3,21 +3,7 @@
 
 let contador_obra = 0;
 let contador_particular = 0;
-let turno_obra = `OBR-${contador_obra}`; //Turno de paciente con Obra Social. VAN DEL 1 AL 99.
-let turno_particular = `PAR-${contador_particular}`; //Turno de paciente Particular.VAN DEL 1 AL 99.
-// let nombre = "";
-// let apellido = "";
-// let dni = "";
-// let motivo_de_consulta = "";
-// let tipo_de_usuario = ""; //OBR (OBRA SOCIAL) o PAR (PARTICULAR)
-// let ingresar_nuevo_usuario = "";
-// let nombre_obra = ""; //Nombre de la Obra Social.
-// let numero_obra = ""; //Número de Obra Social.
-
-// let numero_sala = ""; //Número de Sala al que debe ir el paciente.
-// let input_turno_obr = ""; //input del Turno OBR.
-// let input_turno_par = ""; //input del Turno PAR.
-// let input_sala = ""; //input del Número de Sala.
+let sala = "";
 
 /////////////////FECHA Y HORA///////////////////
 
@@ -57,9 +43,8 @@ function ingresar(){
         let fecha = (`${dia[date.getDay()]}, ${date.getDate()} de ${mes[date.getMonth()]} de ${date.getFullYear()}.`); 
 
         let tipo_de_usuario = document.getElementById("tipo_de_usuario");
-            let value = tipo_de_usuario.value;
 
-            if(value == "Obra Social"){
+            if(tipo_de_usuario.value == "Obra Social"){
 
                 contador_obra++;
 
@@ -69,7 +54,7 @@ function ingresar(){
                     dni: dni.value, 
                     motivo: motivo_de_consulta.value,
                     tipo_de_usuario: tipo_de_usuario.value,
-                    turno_obra: `OBR-${contador_obra}`,
+                    turno: `OBR-${contador_obra}`,
                     nombre_de_obra: nombre_obra.value,
                     numero_de_obra: numero_obra.value,
                     fecha,
@@ -80,7 +65,7 @@ function ingresar(){
                 console.log(`OBR-${contador_obra}`);
             }
 
-            else if(value == "Particular"){
+            else if(tipo_de_usuario.value == "Particular"){
 
                 contador_particular++;
 
@@ -90,7 +75,7 @@ function ingresar(){
                     dni: dni.value, 
                     motivo: motivo_de_consulta.value,
                     tipo_de_usuario: tipo_de_usuario.value,
-                    turno_particular: `PAR-${contador_particular}`,
+                    turno: `PAR-${contador_particular}`,
                     fecha,
                     hora,
                 }
@@ -100,31 +85,137 @@ function ingresar(){
 
             }
             console.log("ARRAY USUARIOS" , array_usuarios);
+            alert("¡Turno creado!"); 
+            insertar_turno_html();
+            listar_turnos();
+
     })
 }
 
-function borrar_turno(e){
-    let form__eliminar = document.getElementById("form__eliminar");
+
+//MUESTRA EL TURNO CREADO RECIENTEMENTE, CON FECHA Y HORA DE INGRESO.
+
+function insertar_turno_html(){ 
+
+    let turno__ingreso = document.getElementById("turno__ingreso"); //MUESTRA TURNO INGRESADO.
+    turno__ingreso.innerHTML = array_usuarios[array_usuarios.length - 1].turno;
+
+    let fecha__ingreso = document.getElementById("fecha__ingreso"); //MUESTRA FECHA DE TURNO INGRESADO.
+    fecha__ingreso.innerHTML = array_usuarios[array_usuarios.length - 1].fecha;
+
+    let hora__ingreso = document.getElementById("hora__ingreso");
+    hora__ingreso.innerHTML = array_usuarios[array_usuarios.length-1].hora;
+}
+
+//LISTA LOS TURNOS EN LA TABLA.
+
+function listar_turnos(){
+
+    //CREO EL TR Y LO INSERTO
+    let tr = document.createElement("tr");
+    tr.setAttribute("class" , "tr_append");
+
+    let tr_padre = document.querySelector("tbody")
+
+    tr_padre.appendChild(tr);
+
+
+    //CREO LOS TD Y LO INSERTO
+
+    let td_turno = document.createElement("td");
+    td_turno.setAttribute("class" , "td_append_turno");
+
+    let td_padre = document.querySelector(".tr_append")
+
+    td_padre.appendChild(td_turno);
+
+    td_sala = document.createElement("td");
+    td_sala.setAttribute("class" , "td_append_sala");
+
+    td_padre = document.querySelector(".tr_append")
+
+    td_padre.appendChild(td_sala);
+
+    //INSERTO EL CONTENIDO DE LOS TD
+
+    td_turno.innerText = array_usuarios[array_usuarios.length-1].turno;
+
+    td_sala.innerText = sala;
+
+}
+
+
+//ELIMINA EL ÚLTIMO TURNO INGRESADO.
+
+function eliminar_ultimo_turno(){ 
+
+    let button_eliminar = document.getElementById("button_eliminar");
     
-    form__eliminar.addEventListener("submit" , function(e){
-        console.log(e.target);
+    button_eliminar.addEventListener("click" , function(e){
+
+        if(array_usuarios.length > 0){
+            alert("¡Turno eliminado!"); 
+            console.log("Turno eliminado:" , array_usuarios[0].turno);
+            array_usuarios.shift();
+            console.log(array_usuarios);
+        }
+        else if(array_usuarios.length === 0){
+            alert("No existen turnos para eliminar.")
+        }
     })
+}
+
+//Se ingresa un turno. Recorre el array hasta que encuentra el objeto con esa propiedad "turno". Lo borra del array.
+
+function eliminar_turno(){
+
+    let input_eliminar = document.getElementById("input__eliminar_turno");
+
+    input_eliminar.addEventListener("change" , function(e){
+        let input_eliminar_value = e.target.value;
+
+        let btn_eliminar_especifico = document.getElementById("btn_eliminar_especifico");
+
+        btn_eliminar_especifico.addEventListener("click" , function(e){
+
+            e.preventDefault();
+            console.log("INPUT:" , input_eliminar_value);
+
+            
+
+        })
+
+    })
+
+}
+
+
+//IMPRIME EL TURNO INGRESADO
+
+function imprimir(){
     
+    let btn__imprimir = document.getElementById("btn__imprimir");
+
+    btn__imprimir.addEventListener("click" , function(){
+
+        let ficha = document.getElementById("ventana__impresion");
+        let ventimp = window.open();
+        ventimp.document.write(ficha.innerHTML);
+        ventimp.document.close();
+        ventimp.print();
+        ventimp.close();
+        console.log("Imprimiendo el Turno ingresado.")
+
+    })
 }
 
-function asignar_sala(){
-
-}
 
 
 //////////////////////////////FUNCIONES/////////////////////////////
-
 ingresar();
-
-
-
-
-
+eliminar_turno();
+eliminar_ultimo_turno();
+imprimir();
 
 
 
