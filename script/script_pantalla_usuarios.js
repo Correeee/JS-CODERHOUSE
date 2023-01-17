02b22cf653f47ee53c8660c1755a9365cc48f498
elcorre <correa755@hotmail.com>
2023-01-17 19:02:19 -0300

    Implementando API

--- script/script_pantalla_usuarios.js
+++ script/script_pantalla_usuarios.js
@@ -1,5 +1,29 @@
+/////SONIDO/////
 let sonido = new Audio("../multimedia/sonido/bell.wav")
 
+//////GEO///////
+
+/////VARIABLES PARA LA API//////
+
+let temperatura;
+
+//////////////API//////////////////
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&units=metric&lang=es&appid=c35e8ea0fba6b3b63fc9262fac9f84c5")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let temperatura = data.main.temp;
+        console.log("Temperatura: " , temperatura);
+        let icono = data.weather[0].icon;
+        console.log(icono)
+
+        document.getElementById("temperatura").innerHTML = `${temperatura}°C`
+    }
+        )
+
+
+        
 
 function turno_actual(){
     let local = JSON.parse(localStorage.getItem("usuarios"));
@@ -15,14 +39,15 @@ function turno_actual(){
 
     padre.append(hijo)
 
+
         if(local?.length > 0 && local[0].sala != "-"){ //Si el local no existe, no tira error del innerHTML.
 
         hijo.innerHTML = `<div class="td_append_turno" id="hijo_append_prop">${local[0].turno}</div><div class="td_append_sala hijo_append_prop" id="hijo_append_${local[0].turno}">${local[0].sala}</div>`
-        
     }
-    else if(local?.length == 0){
-        hijo.innerHTML = "<p class='titulo__noturnos'>NO EXISTEN TURNOS</p>";
-    }
+        else if(local?.length == 0){
+            hijo.innerHTML = "<p class='titulo__noturnos'>NO EXISTEN TURNOS</p>";
+        }
+
 }
 
 function mostrar_hora(){ //MUESTRA Y MANTIENE ACTUALIZADA LA HORA.
@@ -48,8 +73,13 @@ function recargar_turno(){
         if(local?.length > 0 && local[0].sala != "-"){ //Si el local no existe, no tira error del innerHTML. El local[0].sala != "-" hace que no se actualice si la sala no está asignada.
 
         hijo.innerHTML = `<div class="td_append_turno" id="hijo_append_prop">${local[0].turno}</div><div class="td_append_sala hijo_append_prop" id="hijo_append_${local[0].turno}">${local[0].sala}</div>`
+        
     }
+        else if(local?.length == 0){
+            hijo.innerHTML = "<p class='titulo__noturnos'>NO EXISTEN TURNOS</p>";
+        }
 }
+
 
 setInterval( mostrar_hora , 1000); //Actualiza el reloj.
 setInterval (recargar_turno , 1000) //Actualiza el último turno cada 10 segundos.
